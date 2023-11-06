75d70e5958e7312ac45365d4f0c88f135f762c7c
jllp-epfl <javier.lloretpardo@epfl.ch>
2023-11-06 13:23:13 +0100

    now sending multiple parameters to api

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A web application that displays MOOC quizzes and finds related MOOC lectures via the MOOC lecture retrieval API.
 
 
+# Usage
+python3 manage.py runserver 8080
+
--- project/views.py
+++ project/views.py
@@ -1,43 +1,41 @@
-
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 import requests
+from urllib.parse import quote
 
 def index(request):
     return render(request, 'index.html')
 
 @require_POST
 def submit_form(request):
-    # Handle form submission here
-    # For example, you can retrieve the form data and process it
-    # In this example, I'll just return the received data
-    form_data = request.POST
-    # return JsonResponse({'received_data': form_data})
 
-    question = form_data.get('inputField')
+    form_data = request.POST
+
+    question = form_data.get('question')
     print(f'question: {question}')
 
-    headers = {
-        'accept': '*/*',
-    }
-    # url = f'http://127.0.0.1:8000/lectures/?question=<{question}>'
-    # url = f'http://127.0.0.1:8000/<lectures>/<{question}>'
-    url = f'http://127.0.0.1:8000/<{question}>'
+    num_results = form_data.get('num_results')
+    print(f'num_results: {num_results}')
 
-    response = requests.get(url, headers=headers)
+    base_url = 'http://localhost:8000/api/lectures/'
+
+    # Encoding the question parameter using urllib.parse.quote twice to achieve %2520 and %253F
+    encoded_question = quote(quote(question))
+    url = f"{base_url}question/{encoded_question}/num_results/{num_results}/"
+    print(f'url: {url}')
+
+    response = requests.get(url)
     print(f'response: {response}')
+
     data = response.json()
     print(f'data: {data}')
 
-    # video_url = "https://tube.switch.ch/download/video/de94a7a1"
     video_url = data['video_url']
     start_time = data['start_time']
     end_time = data['end_time']
     print(f'start_time: {start_time}')
     print(f'end_time: {end_time}')
 
-
     # solution: https://stackoverflow.com/questions/73403685/how-can-i-get-a-parameter-of-an-url-with-django-rest-framework
-    # /maps/?idfield=<id>,
     return JsonResponse({'received_data': request.POST, 'video_url': video_url, 'start_time': start_time, 'end_time': end_time})
